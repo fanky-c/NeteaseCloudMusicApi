a21a43d723e6e9f0311b5493dce3ee4a78986d3f
zhaochao <zhaochao@rd.netease.com>
2018-02-26 21:54:24 +0800

    add

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
+const {createWebAPIRequest} = require('./util/util');
+
+
+console.log(createWebAPIRequest);
+
 // 跨域设置
-// app.all('*', function (req, res, next) {
-//   res.header("Access-Control-Allow-Credentials", true)
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8")
-//   next()
-// })
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8")
+  next()
+})
 
 app.use(express.static('public'));
 
